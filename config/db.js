2a532f1dea86e5c6e7cc8031f3dc88d8dd0afd80
abhishekmishra <abhishek@oreqo.io>
2025-06-29 13:28:02 +0530

    restart again services

--- config/db.js
+++ config/db.js
@@ -1,7 +1,12 @@
 // const mysql = require("mysql2");
-import mysql from "mysql2";
 import dotenv from "dotenv";
 dotenv.config();
+import mysql from "mysql2";
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+const _filename = fileURLToPath(import.meta.url);
+const _dirname = path.dirname(_filename);
 
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
@@ -9,6 +14,11 @@ const connection = mysql.createConnection({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
+  connectionLimit: 10,
+  queueLimit: 0,
+  ssl: {
+    ca: fs.readFileSync(path.join(_dirname, "../certs/ca.pem")),
+  },
 });
 
 connection.connect((err) => {
