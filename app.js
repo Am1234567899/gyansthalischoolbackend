1d677d9de19a9ec7b9240b892f8f3ab21ab20fe8
abhishekmishra <abhishek@oreqo.io>
2025-06-29 13:17:57 +0530

    restart again services

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
+import dotenv from "dotenv";
+dotenv.config();
+import cors from "cors";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
 import express from "express";
 import db from "./config/db.js";
 const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(bodyParser.json());
+app.use(cookieParser());
 app.get("/login", (req, res) => {
   console.log("Yes dfsd");
   return res.send({ abs: "ok" });
